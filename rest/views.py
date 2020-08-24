42d85b8c970874da34a95f8f4d1fdde7110fe054
Nilesh Bhosale <nilesh.bhosale@birchford.com>
2020-08-24 10:01:23 +0530

    Added basic models and product api

--- rest/views.py
+++ rest/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
-
+from rest.models import Product
+from rest_framework import serializers, viewsets
 
 def index(request):
     return HttpResponse("Hello, world. You're at the rest index.")
+
+
+
+# Serializers define the API representation.
+class ProductSerializer(serializers.Serializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'price']
+
+# ViewSets define the view behavior.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+
